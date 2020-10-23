857937091373d4b9188c3afd9c9fc64d9ddd9783
leejaeseop <jaeseop.lee@nts-corp.com>
2020-10-23 16:38:04 +0900

    프로젝트 설계 & 완성

---
+++ src/today/TodayServlet.java
@@ -0,0 +1,55 @@
+package today;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TodayServlet
+ */
+@WebServlet("/today")
+public class TodayServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public TodayServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		LocalDateTime currentTime = LocalDateTime.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm");
+		out.println("<html>");
+		out.println("<head><title>현재시간</title>");
+		out.println("<link rel=\"stylesheet\" href=\"today.css\">");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<div class=\"layer\">");
+		out.println("<p class=\"p\"><a href=\"index.html\">메인화면</a></p>");
+		out.println("<span class=\"content\">현재시간: ");
+		out.println(currentTime.format(formatter));
+		out.println("</span>");
+		out.println("<span class=\"blank\"</span>");
+		out.println("</div>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+}
