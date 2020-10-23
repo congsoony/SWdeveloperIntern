2e389fad6962be90a47db8735139fc3a4cd1962b
leejaeseop <jaeseop.lee@nts-corp.com>
2020-10-23 23:19:20 +0900

    javadoc 주석삭제

--- src/today/TodayServlet.java
+++ src/today/TodayServlet.java
@@ -11,22 +11,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class TodayServlet
- */
+
 @WebServlet("/today")
 public class TodayServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
-	
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		response.setContentType("text/html;charset=utf-8");
