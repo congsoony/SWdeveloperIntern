762ca5eccbb7040abb9db9fcb755f0032756bfe0
leejaeseop <jaeseop.lee@nts-corp.com>
2020-10-26 11:42:34 +0900

    doGet 메소드 @override 추가

--- src/today/TodayServlet.java
+++ src/today/TodayServlet.java
@@ -15,6 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/today")
 public class TodayServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		response.setContentType("text/html;charset=utf-8");
