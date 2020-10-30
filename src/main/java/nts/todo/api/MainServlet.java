976d94ef1c8b434209726a33c86641e036bf55e1
leejaeseop <jaeseop.lee@nts-corp.com>
2020-10-30 17:46:50 +0900

    필요없는 주석제거 and 각 list별 타입에 맞는 list생성 그리고 requestdispatcher로 forward

--- src/main/java/nts/todo/api/MainServlet.java
+++ src/main/java/nts/todo/api/MainServlet.java
@@ -1,6 +1,8 @@
 package nts.todo.api;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,36 +11,35 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class MainServlet
- */
+import nts.todo.dao.TodoDao;
+import nts.todo.dto.TodoDto;
+
 @WebServlet("/main")
 public class MainServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public MainServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		TodoDao dao=new TodoDao();
+		List<TodoDto> list=dao.getTodos();
+		List<TodoDto> todolist=new ArrayList<>();
+		List<TodoDto> doinglist=new ArrayList<>();
+		List<TodoDto> donelist=new ArrayList<>();
+		for(TodoDto item:list) {
+			if(item.getType()=="TODO")
+				todolist.add(item);
+			else if(item.getType()=="DOING")
+				doinglist.add(item);
+			else
+				donelist.add(item);
+		}
+		request.setAttribute("todolist",todolist);
+		request.setAttribute("doinglist",doinglist);
+		request.setAttribute("donelist",donelist);
+		
 		RequestDispatcher requestDispatcher= request.getRequestDispatcher("/main.jsp");
 		requestDispatcher.forward(request, response);
 		
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
