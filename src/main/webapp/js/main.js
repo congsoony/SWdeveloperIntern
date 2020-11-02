0b5aac81f5b77e6db2302218dff2507558c81008
leejaeseop <jaeseop.lee@nts-corp.com>
2020-11-02 15:03:31 +0900

    frontend 버튼 완성

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -3,31 +3,39 @@ el.addEventListener("click",function(){
 	location.href="register";
 });
 
-
 function Moving(id, type) {
 	var article = document.querySelector(".article_" + id);
 	var nexttype;
+	var target=event.target;
+	console.log(target);
 	if (type == "TODO") {
 		nexttype = "DOING";
 	} else if (type == "DOING") {
-		nextType = "DONE";
+		nexttype = "DONE";
+		
 	}
 	var jsondata = {
 		id : id,
 		type : nexttype
 	};
-
+	
 	var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
         if (xhr.readyState === xhr.DONE) {
             if (xhr.status === 200 || xhr.status === 201) {
                 console.log(xhr.responseText);
+                var curlist=document.querySelector("."+nexttype);
+             	curlist.append(article);
+             	target.setAttribute( 'onclick', "Moving('"+id+"','"+nexttype+"');");
+             	if(nexttype=="DONE")
+             		target.remove();
+            	
             } else {
                 console.error(xhr.responseText);
             }
         }
     };
-
-	req.open("PUT", "type");
-	req.send(JSON.stringify(jsondata));
+	xhr.open("PUT", "type");
+	xhr.send(JSON.stringify(jsondata));	
 }
+
