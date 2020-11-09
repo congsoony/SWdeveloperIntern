f8a631f5b198a8a0f405afa1f5dcaf666fc79e4d
leejaeseop <jaeseop.lee@nts-corp.com>
2020-11-10 00:22:52 +0900

    Service구현

---
+++ src/main/java/kr/or/connect/reservesystem/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,26 @@
+package kr.or.connect.reservesystem.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import kr.or.connect.reservesystem.dao.CategoryDao;
+import kr.or.connect.reservesystem.dto.Category;
+import kr.or.connect.reservesystem.service.CategoryService;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+	@Autowired
+	private CategoryDao categoryDao;
+
+	@Override
+	@Transactional
+	public List<Category> getCategories() {
+		List<Category> list = categoryDao.selectAll();
+		return list;
+	}
+
+}
