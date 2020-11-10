8f42f25ee0dce718a844982e39cedae5f6315482
leejaeseop <jaeseop.lee@nts-corp.com>
2020-11-10 10:32:40 +0900

    category dao, promotiondao 생성

--- src/main/java/kr/or/connect/reservesystem/dao/PromotionDao.java
+++ src/main/java/kr/or/connect/reservesystem/dao/PromotionDao.java
@@ -1,5 +1,29 @@
 package kr.or.connect.reservesystem.dao;
 
-public class PromotionDao {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+import kr.or.connect.reservesystem.dto.Promotion;
+import static kr.or.connect.reservesystem.dao.PromotionDaoSqls.*;
+
+@Repository
+public class PromotionDao {
+	private RowMapper<Promotion> rowMapper = BeanPropertyRowMapper.newInstance(Promotion.class);
+	private NamedParameterJdbcTemplate jdbc;
+
+	public PromotionDao(DataSource dataSource) {
+		this.jdbc = new NamedParameterJdbcTemplate(dataSource);
+	}
+
+	public List<Promotion> selectAll() {
+		Map<String, Integer> params = new HashMap<>();
+		return jdbc.query(SELECT_ALL, params, rowMapper);
+	}
 }
