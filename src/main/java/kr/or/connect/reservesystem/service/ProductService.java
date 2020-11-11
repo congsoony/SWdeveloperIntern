b889da4fc1f4faf4cebaf002a80a3305600e3216
leejaeseop <jaeseop.lee@nts-corp.com>
2020-11-11 18:20:58 +0900

    ProductService추가

--- src/main/java/kr/or/connect/reservesystem/service/ProductService.java
+++ src/main/java/kr/or/connect/reservesystem/service/ProductService.java
@@ -1,5 +1,12 @@
 package kr.or.connect.reservesystem.service;
 
+import java.util.List;
+import kr.or.connect.reservesystem.dto.Product;
+
 public interface ProductService {
 
+	List<Product> getSelectByCategoryId(int start, int categoryId);
+	List<Product> getSelectByAllCategory(int start);
+	int getSelectCategoryCount(int categoryId);
+	int getSelectALLCategoryCount();
 }
