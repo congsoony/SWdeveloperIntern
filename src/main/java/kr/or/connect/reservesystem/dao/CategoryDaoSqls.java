e53958243b6a2965205af958624cc427eecb2bd1
leejaeseop <jaeseop.lee@nts-corp.com>
2020-11-12 18:49:10 +0900

    CategorydaoSql문  수정

--- src/main/java/kr/or/connect/reservesystem/dao/CategoryDaoSqls.java
+++ src/main/java/kr/or/connect/reservesystem/dao/CategoryDaoSqls.java
@@ -1,9 +1,8 @@
 package kr.or.connect.reservesystem.dao;
 
 public class CategoryDaoSqls {
-	public static final String SELECT_ALL ="SELECT b.category_id as id, a.name as name, count(*) as count "
-			+ "FROM category a, product b, display_info c, product_image d, file_info f "
-			+ "where a.id=b.category_id and b.id=c.product_id and a.id=d.product_id and d.file_id=f.id group by b.category_id "
-			+ "order by b.category_id";
-	
+	public static final String SELECT_ALL = "SELECT a.id as id, a.name as name,count(*) as count "
+			+ "FROM category a, product b,display_info c "
+			+ "where a.id=b.category_id and b.id=c.product_id group  by a.id";
+
 }
