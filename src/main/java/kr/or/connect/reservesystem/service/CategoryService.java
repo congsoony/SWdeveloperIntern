0f5c974884460f79d790e4afb5a89667ae866091
이재섭 <jaeseop.lee@nts-corp.com>
2020-11-16 16:13:24 +0900

    Revert "Merge branch 'PJT3_dev' into 'PJT3'"
    This reverts merge request !3

--- src/main/java/kr/or/connect/reservesystem/service/CategoryService.java
+++
@@ -1,8 +0,0 @@
-package kr.or.connect.reservesystem.service;
-
-import java.util.List;
-import kr.or.connect.reservesystem.dto.Category;
-
-public interface CategoryService {
-	List<Category> getCategories();
-}
--- src/main/java/kr/or/connect/reservesystem/service/PromotionService.java
+++
@@ -1,9 +0,0 @@
-package kr.or.connect.reservesystem.service;
-
-import java.util.List;
-
-import kr.or.connect.reservesystem.dto.Promotion;
-
-public interface PromotionService {
-	List<Promotion> getPromotions();
-}
