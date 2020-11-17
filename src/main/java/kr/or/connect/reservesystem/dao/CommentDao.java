e83400cbcfc4fe1451e55c58551d13eda136fb21
leejaeseop <jaeseop.lee@nts-corp.com>
2020-11-17 16:03:54 +0900

    CommentDao 아직 미완성

---
+++ src/main/java/kr/or/connect/reservesystem/dao/CommentDao.java
@@ -0,0 +1,30 @@
+package kr.or.connect.reservesystem.dao;
+
+import static kr.or.connect.reservesystem.dao.sqls.CommentDaoSqls.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import kr.or.connect.reservesystem.dto.Comment;
+import kr.or.connect.reservesystem.dto.Product;
+
+public class CommentDao {
+	private RowMapper<Comment> rowMapper = BeanPropertyRowMapper.newInstance(Comment.class);
+	private NamedParameterJdbcTemplate jdbc;
+
+	public CommentDao(DataSource dataSource) {
+		this.jdbc = new NamedParameterJdbcTemplate(dataSource);
+	}
+	
+	public List<Comment> getCommentList(int displayInfoId) {
+		Map<String, Integer> params = new HashMap<>();
+		return jdbc.query(SELECT_BY_DISPLAY_INFO_ID, params, rowMapper);
+	}
+	
+}
