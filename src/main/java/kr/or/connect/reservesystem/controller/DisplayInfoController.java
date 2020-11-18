935644d5250b5bd3edb518d7476b5b5cf9b5b8f5
leejaeseop <jaeseop.lee@nts-corp.com>
2020-11-18 14:53:33 +0900

    컨트롤러 클래스 생성

--- src/main/java/kr/or/connect/reservesystem/controller/DisplayInfoController.java
+++ src/main/java/kr/or/connect/reservesystem/controller/DisplayInfoController.java
@@ -1,5 +1,34 @@
 package kr.or.connect.reservesystem.controller;
 
+import java.util.HashMap;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import kr.or.connect.reservesystem.dto.DisplayInfo;
+import kr.or.connect.reservesystem.service.DisplayInfoService;
+
+@RestController
+@RequestMapping(path = "/api/displayinfo")
 public class DisplayInfoController {
 
+	@Autowired
+	private DisplayInfoService displayInfoService;
+
+	@GetMapping
+	public Map<String, Object> getDisplayInfo(
+			@RequestParam(name = "displayInfoId", required = true) int displayInfoId) {
+
+		Map<String, Object> map = new HashMap<>();
+		DisplayInfo item = displayInfoService.getDisplayInfo(displayInfoId);
+
+		map.put("displayInfo", item);
+
+		return map;
+	}
 }
--- src/main/java/kr/or/connect/reservesystem/controller/DisplayInfoImageController.java
+++ src/main/java/kr/or/connect/reservesystem/controller/DisplayInfoImageController.java
@@ -1,5 +1,33 @@
 package kr.or.connect.reservesystem.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import kr.or.connect.reservesystem.dto.DisplayInfoImage;
+import kr.or.connect.reservesystem.service.DisplayInfoImageService;
+
+@RestController
+@RequestMapping(path = "/api/displayinfoimage")
 public class DisplayInfoImageController {
+
+	@Autowired
+	private DisplayInfoImageService displayInfoImageService;
+
+	@GetMapping
+	public Map<String, Object> getDisplayInfoImages(@RequestParam(name = "displayInfoId", required = true) int displayInfoId) {
+
+		Map<String, Object> map = new HashMap<>();
+		DisplayInfoImage item = displayInfoImageService.getDisplayInfoImage(displayInfoId);
+
+		map.put("displayInnfoImage", item);
+
+		return map;
+	}
 
 }
