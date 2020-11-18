8b39b86be5b2bbddce65e955fa921189e19728bf
leejaeseop <jaeseop.lee@nts-corp.com>
2020-11-18 14:53:33 +0900

    컨트롤러 클래스 생성

--- src/main/java/kr/or/connect/reservesystem/controller/ProductPriceController.java
+++ src/main/java/kr/or/connect/reservesystem/controller/ProductPriceController.java
@@ -1,5 +1,33 @@
 package kr.or.connect.reservesystem.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import kr.or.connect.reservesystem.dto.ProductPrice;
+import kr.or.connect.reservesystem.service.ProductPriceService;
+
+@RestController
+@RequestMapping(path = "/api/productprices")
 public class ProductPriceController {
 
+	@Autowired
+	private ProductPriceService productPriceService;
+
+	@GetMapping
+	public Map<String, Object> getProductPrices(@RequestParam(name = "displayInfoId", required = true) int displayInfoId) {
+
+		Map<String, Object> map = new HashMap<>();
+		List<ProductPrice> list = productPriceService.getProductPriceList(displayInfoId);
+
+		map.put("productPrices", list);
+
+		return map;
+	}
 }
