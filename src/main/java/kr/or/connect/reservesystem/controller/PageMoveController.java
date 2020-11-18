eb0101451d038190b30d04a2d5ab3f921960c7f7
leejaeseop <jaeseop.lee@nts-corp.com>
2020-11-18 18:54:58 +0900

    페이디이동 컨트롤러생성

---
+++ src/main/java/kr/or/connect/reservesystem/controller/PageMoveController.java
@@ -0,0 +1,18 @@
+package kr.or.connect.reservesystem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class PageMoveController {
+	@RequestMapping(path = "/mainpage",method = RequestMethod.GET)
+	public String moveMainPage() {
+		return "mainpage";
+	}
+	
+	@RequestMapping(path = "/detail",method = RequestMethod.GET)
+	public String moveDtatil() {
+		return "detail";
+	}
+}
