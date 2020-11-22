0a84d879b93987f3edba9975d956378f4a6ecc8f
leejaeseop <jaeseop.lee@nts-corp.com>
2020-11-23 02:53:50 +0900

    totalcount 추가

--- src/main/java/kr/or/connect/reservesystem/controller/DisplayInfoController.java
+++ src/main/java/kr/or/connect/reservesystem/controller/DisplayInfoController.java
@@ -11,10 +11,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import kr.or.connect.reservesystem.dto.Comment;
+import kr.or.connect.reservesystem.dto.CommentImage;
 import kr.or.connect.reservesystem.dto.DisplayInfo;
 import kr.or.connect.reservesystem.dto.DisplayInfoImage;
 import kr.or.connect.reservesystem.dto.ProductImages;
 import kr.or.connect.reservesystem.dto.ProductPrice;
+import kr.or.connect.reservesystem.service.CommentImageService;
 import kr.or.connect.reservesystem.service.CommentService;
 import kr.or.connect.reservesystem.service.DisplayInfoImageService;
 import kr.or.connect.reservesystem.service.DisplayInfoService;
@@ -32,6 +34,8 @@ public class DisplayInfoController {
 	@Autowired
 	private CommentService commentService;
 	@Autowired
+	private CommentImageService commentImageService;
+	@Autowired
 	private ProductPriceService productPriceService;
 	@Autowired
 	private DisplayInfoImageService displayInfoImageService;
@@ -43,13 +47,21 @@ public class DisplayInfoController {
 		DisplayInfo item = displayInfoService.getDisplayInfo(displayInfoId);
 		List<ProductImages> mainImages=productImagesService.getProductImagesList(displayInfoId,"ma");
 		List<ProductImages> etcImages=productImagesService.getProductImagesList(displayInfoId,"et");
+		
 		double averageScore=commentService.getCommentAverage(displayInfoId);
-		List<Comment> comments = commentService.getCommentList(displayInfoId);
+		List<Comment> comments = commentService.getCommentLimitList(displayInfoId);
+		int totalCount=commentService.getCommentTotalCount(displayInfoId);
+		for(Comment it:comments) {
+			
+			List<CommentImage> imglist=commentImageService.getCommentImageList(it.getCommentId());
+			it.setCommentImages(imglist);	
+		}
+		
 		List<ProductPrice> productPrices= productPriceService.getProductPriceList(displayInfoId);
 		DisplayInfoImage displayInfoImage = displayInfoImageService.getDisplayInfoImage(displayInfoId);
 		
 		Map<String, Object> map = new HashMap<>();
-		
+				
 		map.put("displayInfo", item);
 		map.put("mainImages", mainImages);
 		map.put("etcImages", etcImages);
@@ -57,6 +69,7 @@ public class DisplayInfoController {
 		map.put("comments", comments);
 		map.put("productPrices", productPrices);
 		map.put("displayInfoImage", displayInfoImage);
+		map.put("totalCount", totalCount);
 		
 		return map;
 	}
