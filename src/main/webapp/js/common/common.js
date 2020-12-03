455f2e61a2e571360ecf2aa1d2cf7a3aab5b3ce9
leejaeseop <jaeseop.lee@nts-corp.com>
2020-12-03 10:34:11 +0900

    post 넣기

--- src/main/webapp/js/common/common.js
+++ src/main/webapp/js/common/common.js
@@ -28,6 +28,31 @@ function getData(url,callback) {
 	xhr.open("GET", url);
 	xhr.send();
 }
+function postData(url, data) {
+	let xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = ()=> {
+		if (xhr.readyState === xhr.DONE) {
+			if (xhr.status === 200 || xhr.status === 201) {
+				alert("예약완료");
+				location.href=xhr.responseText;
+			} else if(xhr.status === 400|| xhr.status===500){
+				alert("잘못된 경로를 탐색하였습니다. 올바른 값을 입력해주세요");
+				location.href="mainpage";
+			} else if(xhr.status === 403){
+				alert("권한을 가지고 있지않습니다.");
+			} else if(xhr.status === 404){
+				alert("없는 리소스입니다.");
+			} else {
+				alert("서버를 요청할수가 없습니다.");
+			}
+		}
+	};
+	xhr.open("POST", url);
+	xhr.setRequestHeader('Content-Type', 'application/json');
+	xhr.send(JSON.stringify(data));
+}
+
+
 
 function getParameterByName(name) {
     name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
