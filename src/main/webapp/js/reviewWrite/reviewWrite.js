eb32dbf680d38a5536a761719e90406962ec0f65
leejaeseop <jaeseop.lee@nts-corp.com>
2020-12-08 13:58:50 +0900

    댓글달기

--- src/main/webapp/js/reviewWrite/reviewWrite.js
+++ src/main/webapp/js/reviewWrite/reviewWrite.js
@@ -1,7 +1,7 @@
-let reviewWriteObj=null;
+let reviewWriteObj = null;
 document.addEventListener("DOMContentLoaded",()=> {
     goToTopEventListener();
-    reviewWriteObj=new reviewWrite();
+    reviewWriteObj = new reviewWrite();
     reviewWriteObj.hasCookie();
     reviewWriteObj.setEventListener();
     
@@ -9,21 +9,22 @@ document.addEventListener("DOMContentLoaded",()=> {
 });
 
 function reviewWrite(){
-    this.email=getCookie("email");
-    this.starValue=document.getElementById("star_value");
-    this.starList=document.getElementById("star_rating").querySelectorAll("input");
-    this.typingLengthNode=document.getElementById("typing_length");
+    this.email = getCookie("email");
+    this.starValue = document.getElementById("star_value");
+    this.starList = document.getElementById("star_rating").querySelectorAll("input");
+    this.typingLengthNode = document.getElementById("typing_length");
+    this.textAreaNode = document.getElementById("review_textarea");
 }
 
 
 reviewWrite.prototype={
-     hasCookie :function(){
-        if(this.email==null){
+     hasCookie : function(){
+        if(this.email == null){
             alert("잘못된 접근을 하였습니다. 예약한 이메일을 통해 로그인을 해주세요");
             history.back();
         }
     },
-    allDisCheck:function(){
+    allDisCheck : function(){
         this.starList.forEach((item)=>{
             item.checked=false;
         });
@@ -33,11 +34,11 @@ reviewWrite.prototype={
         this.starList.forEach((item)=>{
             item.addEventListener('click',()=>{
                 this.allDisCheck();
-                let value=parseInt(item.value);
-                for(let i=0;i<value;i++){
-                    this.starList[i].checked=true;
+                let value = parseInt(item.value);
+                for(let i = 0;i<value;i++){
+                    this.starList[i].checked = true;
                 }
-                this.starValue.innerText=value;
+                this.starValue.innerText = value;
             });
         });
 
@@ -45,9 +46,28 @@ reviewWrite.prototype={
             evt.currentTarget.style.display="none";
         });
         document.getElementById("review_textarea").addEventListener('keydown',(evt)=>{
-            let target=evt.currentTarget;
-            this.typingLengthNode.innerText=target.value.length;
+            let target = evt.currentTarget;
+            this.typingLengthNode.innerText = target.value.length;
         });
+        document.getElementById("submit_btn").addEventListener('click',()=>{
+            let starScore =  parseFloat(this.starValue.innerText);
+            let textLength = parseInt(this.typingLengthNode.innerText)
+            if(starScore <= 0){
+                alert("별점을 부여해주세요");
+                return;
+            }
+            if(textLength < 5){
+                alert("5자이상 댓글을 남겨주시기 바랍니다.")
+                return;
+            }
+            let data={
+                score:starScore,
+                comment:this.textAreaNode.value,
+                reservationInfoId:getParameterByName("reservationInfoId"),
+                productId:1
+            };
+            postData("api/reviewWrite/onlyComment",data);
+        })
     }
 
 
